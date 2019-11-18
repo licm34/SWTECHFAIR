d637867e3e648e86d8bdb7a19d52f37c07ba3bf1
HyelinNAM <lightsalt28@naver.com>
2019-11-18 22:09:43 +0900

    DB 설정 dbsqlite로 복구

--- BlueMir_WD/settings.py
+++ BlueMir_WD/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = 'BlueMir_WD.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bluemir',
-        'USER': 'root',
-        'PASSWORD':'0208',
-        'HOST':'localhost',
-        'PORT':'3306',
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
